c6b9bc44ac0d453111f2073a8641bd55a1a0c60a
Sheena C <coldbear16@yahoo.com>
2022-05-21 18:51:46 -0400

    completed temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(fahrenheit) {
+  let conversion = (fahrenheit - 32) * 5/9;
+  let rounded = Math.round(conversion *10) / 10;
+return rounded;
 };
 
-const ctof = function() {
-
+const ctof = function(celsius) {
+let conversion = (celsius * 9/5 ) + 32
+let rounded = Math.round(conversion * 10) / 10
+return rounded;
 };
 
 // Do not edit below this line
