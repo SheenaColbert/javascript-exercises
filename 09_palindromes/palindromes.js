91042b56079462bab8f2de60b394ce0597c05187
Sheena C <coldbear16@yahoo.com>
2022-05-21 22:47:32 -0400

    finished palindromes and fibonacci

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (a) { 
+   let filtered = [...a].filter(isLetter);
+   
+   for (let i = 0; i < filtered.length; i++) {
+       const start = filtered[i];
+       const end = filtered[filtered.length - 1 - i];
+       if (start.toLowerCase() !=  end.toLowerCase()) {
+           return false;
+       }
+   }
+   return true;
 };
+
+const isLetter = (a) => {
+    return a.toUpperCase() != a.toLowerCase();
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+if (n < 0) {
+    return "OOPS"
+} 
+let f1 = 0
+let f2 = 0
+let f3 = 1
+if (n == 1 || n == 2){
+    return 1
+}
+while ( n > 1) {
+    f1 = f2
+    f2 = f3
+    f3 = f1 + f2
+    n--
+}
+return f3
 };
 
 // Do not edit below this line
