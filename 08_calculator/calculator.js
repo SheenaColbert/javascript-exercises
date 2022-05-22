4af60a4b232b1661864f7e082760af3c31708b4a
Sheena C <coldbear16@yahoo.com>
2022-05-21 20:39:46 -0400

    finished calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
+const add = function(digit, anotherDigit) {
+	let sum = digit + anotherDigit;
+  return sum;
+};
+
+const subtract = function(a , b) {
+  let answer = a - b;
+  return answer;
 	
 };
 
-const subtract = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++ ) {
+    sum += array[i];
+  }
+	return sum;
 };
 
-const sum = function() {
-	
+const multiply = function(arr) {
+  let sum = 1;
+  for (let i = 0; i < arr.length; i++){
+    sum *= arr[i];
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const power = function(a , b) {
+	let answer = a ** b;
+  return answer;
 };
 
-const power = function() {
-	
-};
+const factorial = function(n) {
+  let product = 1
+	for (let i = 1; i <= n; i++) {
+    product *= i;
+  }
+  return product;
 
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
